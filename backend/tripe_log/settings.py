7e65bbf4f36a19103cdbf33cd336356de0982b86
hassam <hassamhassan.qc@gmail.com>
2026-05-22 21:33:42 +0500

    add docker files

--- backend/tripe_log/settings.py
+++ backend/tripe_log/settings.py
@@ -124,6 +124,9 @@ STATIC_URL = 'static/'
 
 CORS_ALLOWED_ORIGINS = [
     'http://localhost:5173',
+    'http://localhost:5174',
+    'http://127.0.0.1:5173',
+    'http://127.0.0.1:5174',
 ]
 
 REST_FRAMEWORK = {
